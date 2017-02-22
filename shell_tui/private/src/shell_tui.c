7f1e3a02448ed2c55c709ca48976bc88fa117e64
Erjan Altena <erjanaltena@gmail.com>
2017-02-22 22:28:29 +0100

    Adding history to shell_tui

--- shell_tui/private/src/shell_tui.c
+++ shell_tui/private/src/shell_tui.c
@@ -26,35 +26,80 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <termios.h>
+#include <unistd.h>
 #include <sys/time.h>
 #include <sys/select.h>
 
 #include "bundle_context.h"
 #include "bundle_activator.h"
+#include "linked_list.h"
 #include "shell.h"
 #include "shell_tui.h"
 #include "utils.h"
 #include <signal.h>
 #include <unistd.h>
+#include "history.h"
 
+#define LINE_SIZE 256
+#define PROMPT "-> "
+
+// Local types
+
+
+// static function declarations
+static void remove_newlines(char* line);
+
+static void remove_newlines(char* line) {
+    for(int i = 0; i < strlen(line); i++) {
+        if(line[i] == '\n') {
+            for(int j = 0; j < strlen(&line[i]); j++) {
+                line[i+j] = line[i+j+1];
+            }
+        }
+    }
+}
+
+static void clearLine() {
+	printf("\033[2K\r");
+	fflush(stdout);
+}
+
+static void cursorLeft(int n) {
+	if(n>0) {
+		printf("\033[%dD", n);
+		fflush(stdout);
+	}
+}
+
+static void writeLine(const char*line, int pos) {
+    clearLine();
+    write(STDOUT_FILENO, PROMPT, strlen(PROMPT));
+    write(STDOUT_FILENO, line, strlen(line));
+	cursorLeft(strlen(line)-pos);
+}
 
 static void* shellTui_runnable(void *data) {
     shell_tui_activator_pt act = (shell_tui_activator_pt) data;
 
-    char in[256];
-    char dline[256];
-    bool needPrompt = true;
-
+    char in[LINE_SIZE] = "";
+    char buffer[LINE_SIZE];
+    int pos = 0;
+    char dline[LINE_SIZE];
     fd_set rfds;
     struct timeval tv;
 
+    struct termios term_org, term_new;
+    tcgetattr(STDIN_FILENO, &term_org);
+    term_new = term_org;
+    term_new.c_lflag &= ~( ICANON | ECHO);
+    tcsetattr(STDIN_FILENO,  TCSANOW, &term_new);
+
+    history_t *hist = historyCreate();
+
     while (act->running) {
         char * line = NULL;
-        if (needPrompt) {
-            printf("-> ");
-            fflush(stdout);
-            needPrompt = false;
-        }
+        writeLine(in, pos);
         FD_ZERO(&rfds);
         FD_SET(STDIN_FILENO, &rfds);
 
@@ -62,25 +107,106 @@ static void* shellTui_runnable(void *data) {
         tv.tv_usec = 0;
 
         if (select(1, &rfds, NULL, NULL, &tv) > 0) {
-            fgets(in, sizeof(in)-1, stdin);
-            needPrompt = true;
-            memset(dline, 0, 256);
-            strncpy(dline, in, 256);
+        	int nr_chars = read(STDIN_FILENO, buffer, LINE_SIZE-pos);
+            for(int bufpos = 0; bufpos < nr_chars; bufpos++) {
+                if (buffer[bufpos] == '\033' && buffer[bufpos+1] == '[') { //Escape ch  \033 + [ +  A/B/C/D
+                    switch (buffer[bufpos+2]) {
+                        case 'A': // Arrow up
+                            if(historySize(hist) > 0) {
+                                strncpy(in, historyGetPrevLine(hist), LINE_SIZE);
+                                pos = strlen(in);
+                                writeLine(in, pos);
+                            }
+                            break;
+                        case 'B': // Arrow Down
+                            if(historySize(hist) > 0) {
+                                strncpy(in, historyGetNextLine(hist), LINE_SIZE);
+                                pos = strlen(in);
+                                writeLine(in, pos);
+                            }
+                            break;
+                        case 'C': //Arrow right
+                            if (pos < strlen(in)) {
+                        		pos++;
+                        	}
+                    		writeLine(in, pos);
+                        	break;
+                        case 'D': //Arrow left
+                        	if (pos > 0) {
+                            	pos--;
+                            }
+                    		writeLine(in, pos);
+                            break;
+                        case '3':
+                        	if(buffer[bufpos+3] == '~') {
+                        		// delete char
+                                bufpos++; // delete cmd takes 4 chars
+                                int len = strlen(in);
+                                if (pos < len) {
+                                	for (int i = pos; i <= len; i++) {
+                                		in[i] = in[i + 1];
+                                	}
+                                }
+                                writeLine(in, pos);
+                        	}
+                        	break;
+                        default:
+                            printf("Unknown %d : %c\n", nr_chars, buffer[bufpos+2]);
+                            break;
+                    }
+                    bufpos+=2;
+                    continue;
+                } else if(buffer[bufpos] == 127) { // backspace
+                	if(pos > 0) {
+                		int len = strlen(in);
+                		for(int i = pos-1; i <= len; i++) {
+                			in[i] = in[i+1];
+                		}
+                		pos--;
+                	}
+            		writeLine(in, pos);
+            		continue;
+                } else if(buffer[bufpos] != '\n') { // Normal text
+                	if(in[pos] == '\0') {
+                    	in[pos+1] = '\0';
+                	}
+                	in[pos] = buffer[bufpos];
+                    pos++;
+            		writeLine(in, pos);
+                	continue;
+                }
+        		writeLine(in, pos);
+                write(STDOUT_FILENO, "\n", 1);
+                remove_newlines(in);
+                history_addLine(hist, in);
 
-            line = utils_stringTrim(dline);
-            if ((strlen(line) == 0) || (act->shell == NULL)) {
-                continue;
-            }
+                memset(dline, 0, LINE_SIZE);
+                strncpy(dline, in, LINE_SIZE);
 
-            celixThreadMutex_lock(&act->mutex);
-            if (act->shell != NULL) {
-                act->shell->executeCommand(act->shell->shell, line, stdout, stderr);
-            } else {
-                fprintf(stderr, "Shell service not available\n");
-            }
-            celixThreadMutex_unlock(&act->mutex);
+                pos = 0;
+                in[pos] = '\0';
+
+                line = utils_stringTrim(dline);
+                if ((strlen(line) == 0) || (act->shell == NULL)) {
+                    continue;
+                }
+                historyLineReset(hist);
+                celixThreadMutex_lock(&act->mutex);
+                if (act->shell != NULL) {
+                    act->shell->executeCommand(act->shell->shell, line, stdout, stderr);
+                    pos = 0;
+                    nr_chars = 0;
+                    celixThreadMutex_unlock(&act->mutex);
+                    break;
+                } else {
+                    fprintf(stderr, "Shell service not available\n");
+                }
+                celixThreadMutex_unlock(&act->mutex);
+            } // for
         }
     }
+    tcsetattr(STDIN_FILENO,  TCSANOW, &term_org);
+    historyDestroy(hist);
 
     return NULL;
 }
